6a9e5902ca2be3e7b745f8dc7a1f82884c1349e0
Buwei Chen <59496117+BuweiChen@users.noreply.github.com>
2024-01-20 12:46:16 -0500

    Implement number buttons

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
-const numberButtons = [...document.querySelectorAll("buttons .number")];
+const display = document.querySelector("#display");
+const numberButtons = [...document.querySelectorAll(".number")];
 const addButton = document.querySelector("#add");
 const subButton = document.querySelector("#subtract");
 const divButton = document.querySelector("#divide");
 const multButton = document.querySelector("#multiply");
 const eqButton = document.querySelector("#equal");
 
+let displayNumber = "";
 
+for (let bButton of numberButtons) {
+    bButton.addEventListener("click", () => {
+        displayNumber += bButton.getAttribute("data-value");
+        display.textContent = displayNumber;
+    })
+}
 
 function add(a, b) {
     return a + b;
