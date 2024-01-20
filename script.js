143dbfe92d5fcab5a41aaaab9815fa10f38b9dc3
Buwei Chen <59496117+BuweiChen@users.noreply.github.com>
2024-01-20 13:05:50 -0500

    Add operation buttons functionality

--- script.js
+++ script.js
@@ -7,13 +7,27 @@ const multButton = document.querySelector("#multiply");
 const eqButton = document.querySelector("#equal");
 
 let displayNumber = "";
+let calculationResult = 0;
+let lastOperation = "add";
+let eraseOnNextNumber = false;
 
 for (let bButton of numberButtons) {
     bButton.addEventListener("click", () => {
-        displayNumber += bButton.getAttribute("data-value");
+        
+        if (eraseOnNextNumber) {
+            displayNumber = bButton.getAttribute("data-value");
+            eraseOnNextNumber = false;
+        } else {
+            displayNumber += bButton.getAttribute("data-value");
+        }
         display.textContent = displayNumber;
-    })
+    });
 }
+
+addButton.addEventListener("click", operationButtonFunction);
+subButton.addEventListener("click", operationButtonFunction);
+multButton.addEventListener("click", operationButtonFunction);
+divButton.addEventListener("click", operationButtonFunction);
 
 function add(a, b) {
     return a + b;
@@ -29,4 +43,27 @@ function divide(a, b) {
 
 function multiply(a, b) {
     return a * b;
+}
+
+function operationButtonFunction(e) {
+    const n1 = calculationResult;
+    const n2 = Number(displayNumber);
+    switch (lastOperation) {
+        case "add":
+            calculationResult = add(n1, n2);
+            break;
+        case "subtract":
+            calculationResult = subtract(n1, n2);
+            break;
+        case "multiply":
+            calculationResult = multiply(n1, n2);
+            break;
+        case "divide":
+            calculationResult = divide(n1, n2);
+            break;
+    }
+    displayNumber = String(calculationResult);
+    display.textContent = displayNumber;
+    lastOperation = e.target.getAttribute("id");
+    eraseOnNextNumber = true;
 }
